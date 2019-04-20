fe02650396985af8ba8deb951d535b865c9f9294
Thomas Bekhor <tbekhor@Thomass-MacBook-Air.local>
2019-04-20 14:53:56 -0300

    Adicionado combate e outras salas

--- ep1.py
+++ ep1.py
@@ -19,7 +19,7 @@ def carregar_cenarios():
             "descricao": "Voce esta na frente da sala do professor",
             "opcoes": {
                 "inicio": "Tomar o elevador para o saguao de entrada",
-                "de cara com o perigo": "Bater na porta"
+                "Porta da sala": "Bater na porta"
             }
         },
         "Porta da sala": {
@@ -27,8 +27,10 @@ def carregar_cenarios():
             "descricao": "Voce foi pedir para o professor adiar o EP. "
                          "O professor revelou que é um domador de cobras "
                          "e assim, soltou na sala uma grande anaconda, "
-                         "uma mamba negra e sua favorita, a python blackboardiana. Voce foi devorado como Ragnar Lothbrook",
-            "opcoes": {}
+                         "uma mamba negra e sua favorita, a python blackboardiana.",
+            "opcoes": {
+                "sala coordenação: fugir para a sala mestre",
+                "insistir: continuar tentando com El Tochi: domador"}
         },
         "banheiro": {
             "titulo": "Caverna escura",
@@ -36,8 +38,14 @@ def carregar_cenarios():
             "opcoes": {
                 "inicio": "Voltar para o saguão de entrada"
             }
-        }
-    }
+        },
+        "sala coordenação": {
+             "titulo": " Carol da Costa cafofo's",
+             "descricao": "Após nao suceder na sala do El Tochi," 
+                          "voce encontrou a sala de controle total do insper",
+             "opcoes": {
+                 "Entrar computador": "alterar entrega EP"}
+        }}
     nome_cenario_atual = "inicio"
     return cenarios, nome_cenario_atual
 
@@ -56,7 +64,9 @@ def main():
 
     cenarios, nome_cenario_atual = carregar_cenarios()
 
-    player1 = 100 
+    player1 = 100
+    
+    emocional = 100
     
     verdade = True
     
@@ -75,10 +85,17 @@ def main():
         print()
         
         print("Seu total de hitpoints é: {0}".format(player1))
+        
+        print()
+        
+        print("Seu emocional está em : {0}" .format(emocional))
 
         opcoes = cenario_atual['opcoes']
         
         if len(opcoes) == 0:
+            
+            print()
+            
             print("Você foi devorado! Miou pra voce parça... Aiaiaiuiuiui")
             game_over = True
             
@@ -107,9 +124,9 @@ def main():
                     print("-25 hit points")
                     
                     player1 -= 25
-                    
+                    emocional -=10
                     print()
-              
+             
                 
                 if escolha == "sala professor" and verdade:
                     
@@ -117,13 +134,48 @@ def main():
                     
                     print("Você encontrou uma espada: +50 hit points")
                     
-                    print()
-                        
                     player1 += 50
-                        
+                    
+                    print()
+                    
                     verdade = False
+                        
+                    print('O elevador esta lotado, você terá que ir de escada')
+                    
+                    print()
+                    
+                    print('O funcionario da casa do pao de queijo apareceu:')
+                    
+                    print('Ele quer te desafiar pra uma luta pois acabou de ser demitido.')
+                    
+                    print() 
+                    
+                    print('Player 1: {0} hitpoints, {1} de emocional' .format(player1, emocional))
+                    
+                    print('Funcionario demitido: 80 hitpoints, 20 emocional')
+                    
+                    print()
+                    
+                    decisao=input('Voce deseja correr ou lutar?: ')
+                    
+                    if decisao =='lutar':
+                        
+                        print('Player 1 atacou e causou uma morte emocional ao ex-funcionario, coitado!')
+                        
+                    else:
+                        print('Voce é um frangolone')
+                        
+                        print()
+                    
+                        emocional -=15
+                    
                           
             else:
+                
+                print('O adiamento do EP nã foi um sucesso')
+                
+                print()
+                
                 print("Sua indecisão foi sua ruína!")
                 game_over = True
             
